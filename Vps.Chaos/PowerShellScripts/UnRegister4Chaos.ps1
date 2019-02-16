42cfefb733f6d55a5823c607f37a53b24c3b4e3f
Novak <novakbee@gmail.com>
2019-02-16 23:37:53 +0000

    update

--- Vps.Chaos/PowerShellScripts/UnRegister4Chaos.ps1
+++ Vps.Chaos/PowerShellScripts/UnRegister4Chaos.ps1
@@ -1,3 +1,3 @@
 param([string]$ServiceLocation, [string]$ServiceDescription)
 $chaosUrl = "http://localhost:52356/api/Chaos/UnRegister?ServiceLocation=" + $ServiceLocation + "&ServiceDescription=" + $ServiceDescription
-Invoke-RestMethod -Uri $chaosUrl -Method Post -UseDefaultCredential 
+Invoke-RestMethod -Uri $chaosUrl -Method Post -UseDefaultCredential
